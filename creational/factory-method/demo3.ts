b5f5363e8788cfc0f75de3efec06449dedc4a785
YanTee <vanhoinguyen98@gmail.com>
2021-10-16 09:36:57 +0700

    code example for singleton pattern

--- creational/factory-method/demo3.ts
+++ creational/factory-method/demo3.ts
@@ -1,98 +1,70 @@
 namespace FactoryMethodPattern {
-  /**
-   * The Creator class declares the factory method that is supposed to return an
-   * object of a Product class. The Creator's subclasses usually provide the
-   * implementation of this method.
-   */
-  abstract class Creator {
-    /**
-     * Note that the Creator may also provide some default implementation of the
-     * factory method.
-     */
-    public abstract factoryMethod(): Product;
+  interface ITransport {
+    deliver: () => string;
+  }
 
-    /**
-     * Also note that, despite its name, the Creator's primary responsibility is
-     * not creating products. Usually, it contains some core business logic that
-     * relies on Product objects, returned by the factory method. Subclasses can
-     * indirectly change that business logic by overriding the factory method
-     * and returning a different type of product from it.
-     */
-    public someOperation(): string {
-      // Call the factory method to create a Product object.
-      const product = this.factoryMethod();
-      // Now, use the product.
-      return `Creator: The same creator's code has just worked with ${product.operation()}`;
+  class Truck implements ITransport {
+    constructor() {}
+
+    public deliver(): string {
+      return 'Delivery by land with truck';
     }
   }
 
-  /**
-   * Concrete Creators override the factory method in order to change the
-   * resulting product's type.
-   */
-  class ConcreteCreator1 extends Creator {
-    /**
-     * Note that the signature of the method still uses the abstract product
-     * type, even though the concrete product is actually returned from the
-     * method. This way the Creator can stay independent of concrete product
-     * classes.
-     */
-    public factoryMethod(): Product {
-      return new ConcreteProduct1();
+  class MotorBike implements ITransport {
+    constructor() {}
+
+    public deliver(): string {
+      return 'Delivery by land with motorbike';
     }
   }
 
-  class ConcreteCreator2 extends Creator {
-    public factoryMethod(): Product {
-      return new ConcreteProduct2();
+  class Ship implements ITransport {
+    constructor() {}
+
+    public deliver(): string {
+      return 'Delivery by sea with ship';
     }
   }
 
-  /**
-   * The Product interface declares the operations that all concrete products must
-   * implement.
-   */
-  interface Product {
-    operation(): string;
+  interface ILogistics {
+    wheel?: number;
+    createTransport: () => ITransport;
   }
 
-  /**
-   * Concrete Products provide various implementations of the Product interface.
-   */
-  class ConcreteProduct1 implements Product {
-    public operation(): string {
-      return '{Result of the ConcreteProduct1}';
+  class RoadLogistics implements ILogistics {
+    private _wheel: number;
+
+    constructor(wheel = 4) {
+      this._wheel = wheel;
+    }
+
+    public createTransport() {
+      if (this._wheel === 4) {
+        return new Truck();
+      } else {
+        return new MotorBike();
+      }
     }
   }
 
-  class ConcreteProduct2 implements Product {
-    public operation(): string {
-      return '{Result of the ConcreteProduct2}';
+  class SeaLogistics implements ILogistics {
+    constructor() {}
+    public createTransport() {
+      return new Ship();
     }
   }
 
-  /**
-   * The client code works with an instance of a concrete creator, albeit through
-   * its base interface. As long as the client keeps working with the creator via
-   * the base interface, you can pass it any creator's subclass.
-   */
-  function clientCode(creator: Creator) {
-    // ...
-    console.log(
-      "Client: I'm not aware of the creator's class, but it still works."
-    );
-    console.log(creator.someOperation());
-    // ...
-  }
+  (() => {
+    const randomNumber = (min: number, max: number): number => {
+      return Math.floor(Math.random() * max) + min;
+    };
 
-  /**
-   * The Application picks a creator's type depending on the configuration or
-   * environment.
-   */
-  console.log('App: Launched with the ConcreteCreator1.');
-  clientCode(new ConcreteCreator1());
-  console.log('');
+    const type = randomNumber(0, 101);
+    const wheel = type % 4 === 0 ? 4 : 2;
+    const transport =
+      type % 2 === 0 ? new RoadLogistics(wheel) : new SeaLogistics();
 
-  console.log('App: Launched with the ConcreteCreator2.');
-  clientCode(new ConcreteCreator2());
+    console.log(transport.createTransport().deliver());
+  })();
 }
